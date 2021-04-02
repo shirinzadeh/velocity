3f2a1e3908fac98d6e61917226c6a99f823ae6a0
Tahmasib Shirinzade <tshzade@gmail.com>
2021-04-03 01:33:52 +0400

    fixes part2

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@
    
    /* NICE-SELECT LIBRARY FOR SELECT */
    $('select').niceSelect();
+   
    // $('.must').selectpicker();
    // $(".selectpicker").selectpicker();
    // $(".bootstrap-select").click(function () {
@@ -34,8 +35,31 @@
       $('body').removeClass('js-side-dashboard-open');
    });
 
-   $('.js-toggle-dashboard').click(function() {
+   $('.js-toggle-dashboard, .dashboard-compact').click(function() {
       $('body').toggleClass('js-side-dashboard-open');
+   })
+
+   //Hide menu links when click outside account image
+   $(document).mouseup(function (e) {
+      if ($(e.target).closest(user).length === 0) {
+         $(".user-image-dsk .sidebar").hide();
+         $(".sidebar__user-options").hide();
+         $(".sidebar__user i").removeClass("js-animate-icon");
+         $('body').removeClass('js-side-dashboard-open');
+         $('body').removeClass('js-side-menu-open');
+
+      }
+   });
+
+   $('.js-user').click(function() {
+      $('body').addClass('js-side-dashboard-open');
+   })
+
+
+   /* Toggle menu links when click the account image */
+   let user = $('.user-image-dsk');
+   user.click(function() {
+      $('.user-image-dsk .sidebar').toggle();
    })
 
    $('.sidebar__user').click(function() {
@@ -47,45 +71,79 @@
       iconDown.toggleClass('js-animate-icon', userOptions.css('display') == 'block')
    })
 
-   //Toggle menu links when click the account image
-   let user = $('.user-image-dsk');
-   user.click(function() {
-      $('.user-image-dsk .sidebar').toggle();
-   })
-   //Hide menu links when click outside account image
-   $(document).mouseup(function (e) {
-      if ($(e.target).closest(user).length === 0) {
-         $(".user-image-dsk .sidebar").hide();
-         // $('.sidebar__user-options').hide();
-      }
-   });
-
-   $('.js-user').click(function() {
-      $('body').addClass('js-side-dashboard-open');
-   })
-
    /** Selectbox floating label */
-   $('.form-control-category').on('focus blur change', function (e) {
+   $('.form-control-category').each(function(i) {
       var $currEl = $(this);
-     
-     if($currEl) {
-      if($currEl.is('select')) {
-         if($currEl.val() === $("option:first", $currEl).val()) {
-           $('.control-label', $currEl.parent()).animate({opacity: 0, top: '50%',},140);
-          $currEl.parent().removeClass('focused'); 
-          $('.form-control-category > .current').css("color", "rgba(255,255,255,0.5)");
-        } else {
-           $('.control-label', $currEl.parent()).css({top: '-1px', opacity: 1},140);
-          // $('.form-control-category > .current').css("color", "#fff");
- 
-           $currEl.parents('.form-group').toggleClass('focused', ((e.type === 'focus' || this.value.length > 0) && ($currEl.val() !== $("option:first", $currEl).val())));
-        }
-      } else {
-         $currEl.parents('.form-group').toggleClass('focused', (e.type === 'focus' || this.value.length > 0));
-      }
-     }
-   }).trigger('blur');
+
+      $currEl.on('focus blur change', function (e) {
+         if($currEl.is('.form-control-category')) {
+            console.log( $("option[i]:first", $currEl).val())
+            if($currEl.val() === $("option:first", $currEl).val()) {
+               console.log('yes')
+              $('.control-label', $currEl.parent()).animate({opacity: 0, top: '50%',},140);
+              $('.form-control-category > .current').css("color", "rgba(255,255,255,0.5)");
+             $currEl.parent().removeClass('focused'); 
+           } 
+           else {
+              console.log('dfg')
+              $('.control-label', $currEl.parent()).css({top: '-1px', opacity: 1},140);
+             // $('.form-control-category > .current').css("color", "#fff");
+    
+              $currEl.parents('.form-group').toggleClass('focused', ((e.type === 'focus' || this.value.length > 0) && ($currEl.val() !== $("option:first", $currEl).val())));
+           }
+         } else {
+            $currEl.parents('.form-group').toggleClass('focused', (e.type === 'focus' || this.value.length > 0));
+         }
+      }).trigger('blur');
+   })
+   
+
+   
+   $("#checkAll").click(function () {
+      $('#debt1, #debt2, #debt3, #debt4').not(this).prop('checked', this.checked);
+      console.log(this.checked);
+  });
+
+
 })();
+
+/**
+ * SWIPER JS FOR SHOP BRAND IMAGES
+ */
+const swiper = new Swiper('.swiper-container', {
+   slidesPerView: 6,
+   spaceBetween: 42,
+   pagination: {
+     el: '.swiper-pagination',
+     clickable: true,
+   },
+ });
+
+/**
+ * COPY TEXT TO CLIPBOARD
+ */
+ const copyIcon = document.querySelectorAll('.i-copy');
+ const popup = document.querySelector('.popup');
+
+ copyIcon.forEach((icon, i) => {
+    icon.addEventListener('click', () => {
+      let copyText = document.querySelectorAll("#copyToClipboard");
+
+      copyText[i].select();
+      copyText[i].setSelectionRange(0, 99999); /* For mobile devices */
+    
+      // /* Copy the text inside the text field */
+      document.execCommand("copy");
+
+      if(popup) {
+         popup.classList.add("js-show-popup");
+         //remove class when animation end
+         setTimeout(() => {
+            popup.classList.remove("js-show-popup");
+         }, 750); 
+      }
+    })
+ });
 
 /** CHANGE TAB CONTENT WHEN CLICK THE TAB */
 
@@ -133,16 +191,35 @@ changeTabActive(tabsCalcCountry) //Calculator section tabs
 changeTabActive(tabsPackage)
 
 /* Calculator section 
-   CHANGE RANGE VALUE AND BACKGROUND COLOR BEFORE AND AFTER THUMB */
+   
+/** 
+ * RANGE SLIDER
+ */
+
+// CHANGE RANGE SLIDER THUMB ACCORDING TO INPUT VALUE
+
+const rangeVal = document.querySelectorAll('#rangeValue');
+//range.value is input number value
+   rangeVal.forEach((range,i) => {
+      range.addEventListener('keyup', () => {
+         let slider = document.querySelectorAll('#rangeSlider');
+         //change slider which input number is changed
+         slider[i].value = isNaN(parseInt(range.value, 10)) ? 0 : parseInt(range.value, 10);
+
+         let valPercent = (slider[i].valueAsNumber  - parseInt(slider[i].min)) / (parseInt(slider[i].max) - parseInt(slider[i].min));
+         let style = 'background-image: -webkit-gradient(linear, 0% 0%, 100% 0%, color-stop('+ valPercent+', #C9FFC7), color-stop('+ valPercent+', #181C1F));';
+         slider[i].style = style;
+      });
+   });
+
+// CHANGE RANGE VALUE AND BACKGROUND COLOR BEFORE AND AFTER THUMB 
 
 const sliders = document.querySelectorAll("#rangeSlider");
-const output = document.querySelectorAll("#rangeValue");
-
 sliders.forEach(function(slider, i) {   
-   output[i].innerHTML = slider.value;
+   rangeVal[i].defaultValue = slider.value;
 
    slider.oninput =function(){   
-      output[i].innerHTML = this.value; //change value text depends on range value
+      rangeVal[i].defaultValue = this.value; //change value text depends on range value
       //different color before and after thumb while sliding the range
       let valPercent = (slider.valueAsNumber  - parseInt(slider.min)) / (parseInt(slider.max) - parseInt(slider.min));
       let style = 'background-image: -webkit-gradient(linear, 0% 0%, 100% 0%, color-stop('+ valPercent+', #C9FFC7), color-stop('+ valPercent+', #181C1F));';
@@ -151,21 +228,9 @@ sliders.forEach(function(slider, i) {
    slider.oninput();
  });
 
-/** DISPLAY POPUP MESSAGE ON ADDRESSES*/
-const copyIcons = document.querySelectorAll('.i-copy');
-const popup = document.querySelector('.popup')
-
-copyIcons.forEach((icon) => {
-   icon.addEventListener('click', () => {
-      popup.classList.add("js-animation");
-      //remove class when animation end
-      setTimeout(() => {
-         popup.classList.remove("js-animation");
-      }, 2000); 
-   })
-})
-
-// DİSPLAY TOOLTIP
+/**
+ * DİSPLAY TOOLTIP
+ */
 const tooltipInfo = document.querySelectorAll('.tooltip--info');
 const tooltipWarning = document.querySelectorAll('.tooltip--warning');
 const tooltipInfoArrow = document.querySelectorAll('.tooltip--info + .tooltip-arrow');
@@ -195,29 +260,6 @@ tooltipMouseover(warningIcons, tooltipWarning, tooltipWarningArrow);
 tooltipMouseleave(infoIcons, tooltipInfo, tooltipInfoArrow);
 tooltipMouseleave(warningIcons, tooltipWarning, tooltipWarningArrow);
 
-//SIDEBAR
-// const sideUser = document.querySelector('.sidebar__user');
-// const sideUserOptions = document.querySelector('.sidebar__user-options');
-// const iconDown = document.querySelector('.user-info .icon-menu-down_two_tone');
-
-// if(sideUser) {
-//    sideUser.addEventListener('click', () => {
-//       sideUserOptions.style.display = "block";
-//       //change icon when hover
-//       iconDown.style.transform = "rotate(180deg)";
-//       iconDown.style.transition = "all .3s ease";
-//    });
-
-//    let hideUserOptions =  function() {
-//       sideUserOptions.style.display = "none";
-//       iconDown.style.transform = "rotate(0deg)";
-//    }
-
-//    // sideUser.addEventListener('mouseleave', hideUserOptions);
-//    sideUser.addEventListener('click', hideUserOptions);
-// }
-
-
 /** TOGGLE PASSWORD VISIBILITY */
 const password = document.querySelectorAll('#password');
 const togglePassword = document.querySelectorAll('#togglePassword');
@@ -233,17 +275,3 @@ togglePassword.forEach((pw, i) => {
 
    })
 });
-
-onclick="this.setAttribute('value', document.querySelector('.nice-select .current').innerHTML); console.log(this)"
-
-// const selects = document.querySelectorAll('.nice-select.floating-select');
-// const selectValue = document.querySelector('.nice-select.floating-select .current');
-// const listitem = document.querySelector('.nice-select.floating-select li[data-value]')
-// selects.forEach((select, i) => {
-//    select.addEventListener('click', () => {
-//       console.log(selectValue)
-//       if(selectValue.length >= 1) {
-//          console.log('fg')
-//       }
-//    })
-// })
