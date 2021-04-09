ec06d7feb68324761a61cf7af0f6594a8fa88056
Tahmasib Shirinzade <tshzade@gmail.com>
2021-04-09 14:38:17 +0400

    structuring folders

--- js/script.js
+++ js/script.js
@@ -91,6 +91,28 @@
       }
     }
 
+    // SELECT ALL CHECKBOXES
+     $('#selectAll').on('click',function(){
+        console.log('dfg')
+        if(this.checked){
+            $('.checkbox').each(function(){
+                this.checked = true;
+            });
+        }else{
+             $('.checkbox').each(function(){
+                this.checked = false;
+            });
+        }
+    });
+    
+    $('.checkbox').on('click',function(){
+        if($('.checkbox:checked').length == $('.checkbox').length){
+            $('#select_all').prop('checked',true);
+        }else{
+            $('#select_all').prop('checked',false);
+        }
+    });
+
    /**SELECT 2 PLUGIN */
 
    /** Multiple selectbox */
